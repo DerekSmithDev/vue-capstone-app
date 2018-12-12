ad5e0e1b83d8367aea3dbd4a6c1ae1ed652dbb69
dereksmith83 <mdsmith1983@gmail.com>
2018-12-11 20:00:46 -0600

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="home">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!"
+    };
+  },
+  created: function() {},
+  methods: {},
+  computed: {}
+};
+</script>
+<!-- <template>
+  <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -16,3 +37,4 @@ export default {
   }
 }
 </script>
+ -->
